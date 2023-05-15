5fa52ab2efaa944734a37288c10b2f8e11a60044
Daryii <112935597+Daryii@users.noreply.github.com>
2023-05-15 18:10:14 +0200

    commit

--- Eind-opdracht/index.php
+++ Eind-opdracht/index.php
@@ -4,13 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eind_Opdracht</title>
     <link rel="stylesheet" href="style.css">
+    <title>Eind_opdracht</title>
+    
 </head>
 <body>
-    <p></p>
+    
     <?php
+    function fotoVerander($imagePath){
+        $imagePath = '/var/www/Eind-opdracht/afternoon.png'; // Replace with the actual path to your image file
+   
+        // Read the image file and assign its contents to a variable
+        $imageData = file_get_contents($imagePath);
         
-    ?>
+        // Output the image data as base64
+        $imageBase64 = base64_encode($imageData);
+     
+    }
+
+   
+        $tijd = date('H:i:s', time());
+       
+
+    if ( $tijd == 00:06:00 ){
+        fotoVerander($imageBase64)
+    }   
+
+   ?>
+<!--    
+   <img src="data:image/png;base64,<?php echo $imageBase64; ?>" alt="Image">
+    -->
+   
+
 </body>
 </html>
