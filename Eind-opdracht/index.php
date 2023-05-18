56c8080f731ade4538e9af1795bd30ff321b2d04
Daryii <112935597+Daryii@users.noreply.github.com>
2023-05-18 15:17:28 +0200

    commit

--- Eind-opdracht/index.php
+++ Eind-opdracht/index.php
@@ -6,35 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Eind_opdracht</title>
-    
 </head>
 <body>
-    
     <?php
-    function fotoVerander($imagePath){
-        $imagePath = '/var/www/Eind-opdracht/afternoon.png'; // Replace with the actual path to your image file
-   
+    function fotoVerander($imagePath)
+    {
         // Read the image file and assign its contents to a variable
         $imageData = file_get_contents($imagePath);
-        
+
         // Output the image data as base64
         $imageBase64 = base64_encode($imageData);
-     
+
+        return $imageBase64;
     }
 
-   
-        $tijd = date('H:i:s', time());
-       
+    date_default_timezone_set('Europe/Amsterdam');
+    $tijd = date('H:i:s', time());
 
-    if ( $tijd == 00:06:00 ){
-        fotoVerander($imageBase64)
-    }   
+    $imageBase64 = '';
 
-   ?>
-<!--    
-   <img src="data:image/png;base64,<?php echo $imageBase64; ?>" alt="Image">
-    -->
-   
+    if ($tijd >= '00:06:00' && $tijd <= '00:12:00') {
+        $imagePath = '/var/www/Eind-opdracht/image/morning.png';
+        $imageBase64 = fotoVerander($imagePath);
+    } else if ($tijd >= '00:12:00' && $tijd <= '00:18:00') {
+        $imagePath = '/var/www/Eind-opdracht/image/afternoon.png';
+        $imageBase64 = fotoVerander($imagePath);
+    }
+    ?>
 
+    <img src="data:image/png;base64,<?php echo $imageBase64; ?>" alt="Image">
 </body>
 </html>
