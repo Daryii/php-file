c00a38e22cf6f9495b74787ef9a579f589e6f458
Daryii <112935597+Daryii@users.noreply.github.com>
2023-06-09 13:05:10 +0200

    commit

--- welcome/index.php
+++ welcome/index.php
@@ -5,20 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>welcome</title>
+    <title>Welcome</title>
 </head>
 <body>
+    <h1>De ingevulde gegevens zijn:</h1>
+    
 <?php
-    if (isset($_GET['naam']) && isset($_GET['email'])) {
-        $naam = $_GET['naam'];
-        $email = $_GET['email'];
 
-        echo "<h1>De ingevulde gegevens zijn:</h1>";
-        echo 'Naam: ' . $naam . "<br>";
-        echo 'E-mail: ' . $email;
-    } else {
-        echo "<h1>Geen gegevens ontvangen</h1>";
-    }
-    ?>
+
+    $name = $_GET['naam'];
+    $email = $_GET['email'];
+
+
+    echo "naam: " . $name . "<br>";
+    echo "email: " . $email . "<br>";
+
+?>
+
+</script>
 </body>
 </html>
