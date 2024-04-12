6dc46a0393b09cb6d18f76fc7a03de4eccbe093e
Daryii <112935597+Daryii@users.noreply.github.com>
2024-04-12 15:26:57 +0200

    commit

--- C2/Render.py
+++ C2/Render.py
@@ -1,14 +1,41 @@
-def generate_form():
+def generate_form(input_types):
     form_html = "<form method='post' action='handle_form.php'>"
-    form_html += "<input type='text' name='name' placeholder='Naam'><br>"
-    form_html += "<input type='email' name='email' placeholder='E-mail'><br>"
-    form_html += "<input type='password' name='password' placeholder='Wachtwoord'><br>"
-    form_html += "<select name='country'><option value='NL'>Nederland</option><option value='DE'>Duitsland</option><option value='FR'>Frankrijk</option></select><br>"
-    form_html += "<input type='checkbox' name='driving_license' value='yes'>Rijbewijs<br>"
-    form_html += "<input type='date' name='birthdate' placeholder='Geboortedatum'><br>"
+
+    for input_type, input_count in input_types.items():
+        for i in range(input_count):
+            if input_type == 'text':
+                form_html += f"<input type='text' name='text_input_{i+1}' placeholder='Tekstveld'><br>"
+            elif input_type == 'password':
+                form_html += f"<input type='password' name='password_input_{i+1}' placeholder='Wachtwoordveld'><br>"
+            elif input_type == 'email':
+                form_html += f"<input type='email' name='email_input_{i+1}' placeholder='E-mailveld'><br>"
+            elif input_type == 'dropdown':
+                form_html += "<select name='dropdown_input'>"
+                form_html += "<option value='NL'>Nederland</option>"
+                form_html += "<option value='DE'>Duitsland</option>"
+                form_html += "<option value='FR'>Frankrijk</option>"
+                form_html += "</select><br>"
+            elif input_type == 'checkbox':
+                form_html += f"<input type='checkbox' name='checkbox_input_{i+1}'>Checkbox {i+1}<br>"
+            elif input_type == 'datepicker':
+                form_html += f"<input type='date' name='datepicker_input_{i+1}'><br>"
+    
     form_html += "<input type='submit' value='Verzenden'></form>"
     return form_html
 
-# Generate the form
-generated_form = generate_form()
+def get_input_types():
+    input_types = {}
+    while True:
+        input_type = input("Welk type input wilt u toevoegen (text/password/email/dropdown/checkbox/datepicker)? Voer 'stop' in om te stoppen: ").lower()
+        if input_type == 'stop':
+            break
+        input_count = int(input("Hoeveel van dit type input wilt u toevoegen? "))
+        input_types[input_type] = input_count
+    return input_types
+
+# Vraag de gebruiker welke formulierelementen ze willen toevoegen
+input_types = get_input_types()
+
+# Genereer het formulier op basis van de gebruikersinvoer
+generated_form = generate_form(input_types)
 print(generated_form)
--- C2/handle_form.php
+++ C2/handle_form.php
@@ -1,40 +1,25 @@
 <?php
 
-// Check if the form is submitted
+// Controleer of het formulier is ingediend
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if all fields are filled
-    if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['country']) && isset($_POST['driving_license']) && isset($_POST['birthdate'])) {
-        // Get the submitted data
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $country = $_POST['country'];
-        $driving_license = $_POST['driving_license'];
-        $birthdate = $_POST['birthdate'];
+    // Array om de ingevulde gegevens op te slaan
+    $form_data = array();
 
-        // Create an associative array of the data
-        $data = array(
-            'name' => $name,
-            'email' => $email,
-            'password' => $password,
-            'country' => $country,
-            'driving_license' => $driving_license,
-            'birthdate' => $birthdate
-        );
-
-        // Convert the array to JSON format
-        $json_data = json_encode($data);
-
-        // Write the JSON data to a local file
-        $file_path = 'user_data.json';
-        file_put_contents($file_path, $json_data);
-
-        // Display the JSON data on the screen
-        echo $json_data;
-    } else {
-        // Display an error message if not all fields are filled
-        echo "Error: Niet alle velden zijn ingevuld";
+    // Loop door elk ingediend veld en sla de waarde op
+    foreach ($_POST as $key => $value) {
+        // Voeg het veld toe aan de form_data array
+        $form_data[$key] = $value;
     }
+
+    // Converteer de array naar JSON-formaat
+    $json_data = json_encode($form_data);
+
+    // Schrijf de JSON-gegevens naar een lokaal bestand
+    $file_path = 'user_data.json';
+    file_put_contents($file_path, $json_data);
+
+    // Geef de JSON-gegevens weer op het scherm
+    echo $json_data;
 }
 
 ?>
